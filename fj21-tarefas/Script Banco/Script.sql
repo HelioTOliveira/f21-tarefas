a6bde36b2ff2087648c757881fdb36f36df73e37
helio.oliveira <helio.oliveira@empresa>
2017-07-04 12:56:35 -0300

    11.9 Execício: Criando Tarefa
    Obs: O projeto esta com erro ao execultar não acha a pagina

---
+++ fj21-tarefas/Script Banco/Script.sql
@@ -0,0 +1,13 @@
+mysql -u root
+
+use fj21;
+
+create table tarefas (
+
+id BIGINT NOT NULL AUTO_INCREMENT,
+descricao VARCHAR(255),
+finalizado BOOLEAN,
+dataFinalizacao DATE,
+primary key (id)
+
+);
