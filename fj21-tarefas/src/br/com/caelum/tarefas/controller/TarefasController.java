656ecef6e6975fe0b4f3a96d5aae87ea7dd5240b
helio.oliveira <helio.oliveira@empresa>
2017-07-05 12:45:21 -0300

    11.12 Exercícios: Validando Tarefas OBS: Verificar os jar depois pois esta dando erro

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -1,6 +1,9 @@
 package br.com.caelum.tarefas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
@@ -15,7 +18,12 @@ public class TarefasController {
 		return "tarefa/formulario";
 	}
 	
-	public String adicionar(Tarefa tarefa){
+	@RequestMapping("adicionaTarefa")
+	public String adicionar(@Valid Tarefa tarefa, BindingResult result){
+		
+		if(result.hasFieldErrors("descricao")) {
+			return "tarefa/formulario";
+			}
 		
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
